7a445b7b6a89946295d4b96dbf5d22019e884db1
André Cordeiro <ahdncordeiro@gmail.com>
2020-04-09 16:50:46 -0300

    Update

--- bitbucket-comments-scroll-keyboard-shortcuts.js
+++ bitbucket-comments-scroll-keyboard-shortcuts.js
@@ -1,13 +1,14 @@
 var shouldAddEventListener;
 var comments;
 var currentCommentIndex;
+
 // define a handler
 function doc_keyUp(e) {
   // this would test if keyCode 220 (\) was pressed
   if (e.keyCode == 220) { // Test if keyCode 220 (\) was pressed
     console.log("Keyboard for next comment triggered.");
     comments[currentCommentIndex].scrollIntoView({behavior: 'smooth'});
-    currentCommentIndex = currentCommentIndex == (comments.length - 1) ? 0 : currentCommentIndex + 1;
+    currentCommentIndex = (currentCommentIndex == (comments.length - 1)) ? 0 : (currentCommentIndex + 1);
   }
 }
 
