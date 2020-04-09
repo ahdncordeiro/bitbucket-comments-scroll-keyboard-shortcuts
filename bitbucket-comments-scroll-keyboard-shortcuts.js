bedddc605da2f7cd3f94e4ea3e024ac15f2cf876
André Cordeiro <ahdncordeiro@gmail.com>
2020-04-09 16:57:36 -0300

    Fix when html is rendered later than script is injected

--- bitbucket-comments-scroll-keyboard-shortcuts.js
+++ bitbucket-comments-scroll-keyboard-shortcuts.js
@@ -7,16 +7,21 @@ function doc_keyUp(e) {
   // this would test if keyCode 220 (\) was pressed
   if (e.keyCode == 220) { // Test if keyCode 220 (\) was pressed
     console.log("Keyboard for next comment triggered.");
-    comments[currentCommentIndex].scrollIntoView({behavior: 'smooth'});
-    currentCommentIndex = (currentCommentIndex == (comments.length - 1)) ? 0 : (currentCommentIndex + 1);
+    if (comments.length == 0) {
+      comments = document.querySelectorAll("[class='comment-thread-container']");
+    }
+    if (comments.length > 0) {
+      comments[currentCommentIndex].scrollIntoView({behavior: 'smooth'});
+      currentCommentIndex = (currentCommentIndex == (comments.length - 1)) ? 0 : (currentCommentIndex + 1);
+    }
   }
 }
 
 // register the handler 
 if ((shouldAddEventListener == null ? true : shouldAddEventListener)) {
   shouldAddEventListener = false;
-  console.log("Adding Bitbucket [Go to next comment] keyboard shortcuts event listener...")
-  comments = document.querySelectorAll("[class='comment-thread-container']")
+  console.log("Adding Bitbucket [Go to next comment] keyboard shortcuts event listener...");
+  comments = document.querySelectorAll("[class='comment-thread-container']");
   currentCommentIndex = 0;
   document.addEventListener('keyup', doc_keyUp, false);
 }
